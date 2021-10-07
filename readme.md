aca8c37d85a41acd41d9eec766df9e185ed4fe69
Seb-HB <sfouvet@stagiaire-humanbooster.com>
2021-10-07 09:49:30 +0200

    avant token

---
+++ readme.md
@@ -0,0 +1,20 @@
+1) Les verbes
+GET: Permet d'accèder à une ressource ou appeller une fonction ()
+POST: Permet d'ajouter une ressource ou exécuter une action
+PUT: Met à jour une ressource complète en la remplaçant par une nouvelle version (équivalent update)
+DELETE: pour supprimer une ressource à partir de sa clé primaire
+HEAD: Equivalent au GET sauf qu’aucune ressource n’est retournée
+
+
+2) composant de la requete
+
+-URL
+-HEADERS
+-METHODE (verbe HTTP)
+-Un Body (optionnel, surtout utilisé avec les methodes post put et delete)
+
+3) la réponse
+
+-HEADERS
+-BODY
+-Un code statut (resultat de la requete)
---
+++ src/Entity/Film.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\FilmRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=FilmRepository::class)
+ */
+#[ApiResource]
+class Film
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $titre;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $realisateur;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getRealisateur(): ?string
+    {
+        return $this->realisateur;
+    }
+
+    public function setRealisateur(string $realisateur): self
+    {
+        $this->realisateur = $realisateur;
+
+        return $this;
+    }
+}
